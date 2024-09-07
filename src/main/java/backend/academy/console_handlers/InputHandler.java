c268b0027ca04696b039eb582b70b50ead9101fb
oleynik-n-a <naoleynik4@gmail.com>
2024-09-07 19:35:58 +0300

    fix: change InputHandler singleton logic, add opportunity to choose InputStream

--- src/main/java/backend/academy/console_handlers/InputHandler.java
+++ src/main/java/backend/academy/console_handlers/InputHandler.java
@@ -1,23 +1,24 @@
 package backend.academy.console_handlers;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 public final class InputHandler {
     private static final InputHandler INSTANCE = new InputHandler();
-    private final Scanner scanner;
+    private static Scanner SCANNER;
 
     private InputHandler() {
-        scanner = new Scanner(System.in);
     }
 
-    public static InputHandler getInstance() {
+    public static InputHandler getInstance(InputStream inputStream) {
+        SCANNER = new Scanner(inputStream);
         return InputHandler.INSTANCE;
     }
 
     public int readInteger() {
         while (true) {
             try {
-                return Integer.parseInt(scanner.nextLine());
+                return Integer.parseInt(SCANNER.nextLine());
             } catch (NumberFormatException e) {
                 continue;
             }
@@ -27,7 +28,7 @@ public final class InputHandler {
     public char readLetter() {
         String input;
         while (true) {
-            input = scanner.nextLine();
+            input = SCANNER.nextLine();
             if (input.length() != 1) {
                 continue;
             }
