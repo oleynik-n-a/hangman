519aa343b4e3613a387735dd05fbcce932ab1049
oleynik-n-a <naoleynik4@gmail.com>
2024-09-07 19:42:18 +0300

    fix: change name - SCANNER -> scanner

--- src/main/java/backend/academy/console_handlers/InputHandler.java
+++ src/main/java/backend/academy/console_handlers/InputHandler.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 
 public final class InputHandler {
     private static final InputHandler INSTANCE = new InputHandler();
-    private static Scanner SCANNER;
+    private static Scanner scanner;
 
     private InputHandler() {
     }
 
     public static InputHandler getInstance(InputStream inputStream) {
-        SCANNER = new Scanner(inputStream);
+        scanner = new Scanner(inputStream);
         return InputHandler.INSTANCE;
     }
 
     public int readInteger() {
         while (true) {
             try {
-                return Integer.parseInt(SCANNER.nextLine());
+                return Integer.parseInt(scanner.nextLine());
             } catch (NumberFormatException e) {
                 continue;
             }
@@ -28,7 +28,7 @@ public final class InputHandler {
     public char readLetter() {
         String input;
         while (true) {
-            input = SCANNER.nextLine();
+            input = scanner.nextLine();
             if (input.length() != 1) {
                 continue;
             }
