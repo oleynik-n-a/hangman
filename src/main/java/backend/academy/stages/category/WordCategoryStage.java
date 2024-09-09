e3307cc757ae2bfa13676840ca70dd60671a78fb
oleynik-n-a <naoleynik4@gmail.com>
2024-09-09 12:06:34 +0300

    fix(WordCategoryStage): edit code style

--- src/main/java/backend/academy/stages/category/WordCategoryStage.java
+++ src/main/java/backend/academy/stages/category/WordCategoryStage.java
@@ -1,13 +1,14 @@
 package backend.academy.stages.category;
 
-import backend.academy.adapters.IRandom;
 import backend.academy.adapters.IPrintable;
+import backend.academy.adapters.IRandom;
 import backend.academy.game_states.WordCategoryOptions;
 import org.apache.commons.lang3.NotImplementedException;
 
 public class WordCategoryStage implements IPrintable, IRandom {
     private WordCategoryOptions wordCategory;
 
+    @SuppressWarnings("magicnumber")
     public void submitCategory(int input) {
         switch (input) {
             case 1:
@@ -22,8 +23,9 @@ public class WordCategoryStage implements IPrintable, IRandom {
             case 4:
                 wordCategory = WordCategoryOptions.SPORTS;
                 break;
-            case 5:
-                submitCategory(IRandom.generateRandomInteger(1, 4));
+            default:
+                wordCategory = WordCategoryOptions.RANDOM;
+                submitCategory(IRandom.generateRandomInteger(1, WordCategoryOptions.values().length - 1));
                 break;
         }
     }
@@ -33,14 +35,15 @@ public class WordCategoryStage implements IPrintable, IRandom {
     }
 
     @Override
+    @SuppressWarnings("multiplestringliterals")
     public String getPrintableView() {
-        return "Choose word category:\n" +
-            "  1. " + WordCategoryOptions.ANIMALS.name() + ".\n" +
-            "  2. " + WordCategoryOptions.FRUITS.name() + ".\n" +
-            "  3. " + WordCategoryOptions.HOUSEHOLD_APPLIANCES.name() + ".\n" +
-            "  4. " + WordCategoryOptions.SPORTS.name() + ".\n" +
-            "  5. Skip.\n\n" +
-            "Input here:\n" +
-            "  ->";
+        return "Choose word category:\n"
+            + "  1. " + WordCategoryOptions.ANIMALS.name() + ".\n"
+            + "  2. " + WordCategoryOptions.FRUITS.name() + ".\n"
+            + "  3. " + WordCategoryOptions.HOUSEHOLD_APPLIANCES.name() + ".\n"
+            + "  4. " + WordCategoryOptions.SPORTS.name() + ".\n"
+            + "  5. Skip (random category)\n\n"
+            + "Input here:\n"
+            + "  ->";
     }
 }
