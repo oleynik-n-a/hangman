d7d5e7031d114e91344f97a04b008ec345750ffa
oleynik-n-a <naoleynik4@gmail.com>
2024-10-13 15:45:09 +0300

    feat: move huge logic blocks from main to special StagesRunner class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,13 +1,6 @@
 package backend.academy;
 
-import backend.academy.stages.category.WordCategoryOption;
-import backend.academy.stages.category.WordCategoryStage;
-import backend.academy.stages.difficulty.CustomDifficultyStage;
-import backend.academy.stages.difficulty.GameDifficultyOption;
-import backend.academy.stages.difficulty.GameDifficultyStage;
-import backend.academy.stages.game.GameSession;
-import backend.academy.stages.game.GameSessionResults;
-import backend.academy.stages.game.GameStepOption;
+import backend.academy.stages.StagesRunner;
 import backend.academy.stream_handlers.InputHandler;
 import backend.academy.stream_handlers.PrintHandler;
 import lombok.experimental.UtilityClass;
@@ -19,55 +12,20 @@ public class Main {
         PrintHandler printHandler = PrintHandler.getInstance(System.out);
         InputHandler inputHandler = InputHandler.getInstance(System.in);
 
-        // Initializing Builder class for future game session (builder pattern).
-        final var gameSessionBuilder = GameSession.builder();
-
-        int input;
+        final var stagesRunner = new StagesRunner(printHandler, inputHandler);
 
         // Game cycle to make possible restart the game.
         while (true) {
             // Stage to choose word category.
-            WordCategoryStage wordCategoryStage = new WordCategoryStage();
-            printHandler.printView(wordCategoryStage);
-            input = inputHandler.readInteger(printHandler, 1, WordCategoryOption.values().length);
-            wordCategoryStage.submitCategory(input);
-
-            // Building game session after choosing stage.
-            gameSessionBuilder.wordInfo(wordCategoryStage.generateWord(WordCategoryStage.DICTIONARY));
+            stagesRunner.RunCategoryStages();
 
             // Stage to choose game difficulty (lives amount).
-            GameDifficultyStage gameDifficultyStage = new GameDifficultyStage();
-            printHandler.printView(gameDifficultyStage);
-            input = inputHandler.readInteger(printHandler, 1, GameDifficultyOption.values().length);
-            gameDifficultyStage.submitDifficulty(input);
-
-            // Building game session after choosing stage.
-            gameSessionBuilder.difficulty(gameDifficultyStage.gameDifficulty());
-
-            // Stage to choose game custom difficulty (custom lives amount).
-            if (gameDifficultyStage.gameDifficulty() == GameDifficultyOption.CUSTOM) {
-                gameDifficultyStage = new CustomDifficultyStage();
-                printHandler.printView(gameDifficultyStage);
-                input = inputHandler.readInteger(printHandler, 1, GameDifficultyStage.MAX_LIVE_AMOUNT);
-                gameDifficultyStage.submitDifficulty(input);
-            }
-
-            // Building game session after choosing stage.
-            gameSessionBuilder.lives(gameDifficultyStage.getLivesAmount());
+            stagesRunner.RunDifficultyStages();
 
             // Game session building done, starting game.
-            GameSession gameSession = gameSessionBuilder.build();
-            printHandler.printView(gameSession);
-            GameStepOption gameStep = GameStepOption.GAMING_STEP;
-            while (gameStep != GameStepOption.RESULTING_STEP) {
-                char inputCharacter = inputHandler.readLetter(printHandler);
-                gameStep = gameSession.executeGameStep(inputCharacter);
-                printHandler.printView(gameSession);
-            }
+            stagesRunner.RunGameStages();
 
             // End of the game.
-            GameSessionResults gameSessionResults = new GameSessionResults(gameSession.getGameResults());
-            printHandler.printView(gameSessionResults);
             if (inputHandler.readLetter(printHandler) != 'Y') {
                 return;
             }
---
+++ src/main/java/backend/academy/stages/StagesRunner.java
@@ -0,0 +1,67 @@
+package backend.academy.stages;
+
+import backend.academy.stages.category.WordCategoryOption;
+import backend.academy.stages.category.WordCategoryStage;
+import backend.academy.stages.difficulty.CustomDifficultyStage;
+import backend.academy.stages.difficulty.GameDifficultyOption;
+import backend.academy.stages.difficulty.GameDifficultyStage;
+import backend.academy.stages.game.GameSession;
+import backend.academy.stages.game.GameSessionResults;
+import backend.academy.stages.game.GameStepOption;
+import backend.academy.stream_handlers.InputHandler;
+import backend.academy.stream_handlers.PrintHandler;
+
+public class StagesRunner {
+    public StagesRunner(PrintHandler printHandler, InputHandler inputHandler) {
+        this.printHandler = printHandler;
+        this.inputHandler = inputHandler;
+    }
+
+    public void RunCategoryStages() {
+        WordCategoryStage wordCategoryStage = new WordCategoryStage();
+        printHandler.printView(wordCategoryStage);
+        int input = inputHandler.readInteger(printHandler, 1, WordCategoryOption.values().length);
+        wordCategoryStage.submitCategory(input);
+
+        // Building game session after choosing stage.
+        gameSessionBuilder.wordInfo(wordCategoryStage.generateWord(WordCategoryStage.DICTIONARY));
+    }
+
+    public void RunDifficultyStages() {
+        GameDifficultyStage gameDifficultyStage = new GameDifficultyStage();
+        printHandler.printView(gameDifficultyStage);
+        int input = inputHandler.readInteger(printHandler, 1, GameDifficultyOption.values().length);
+        gameDifficultyStage.submitDifficulty(input);
+
+        // Building game session after choosing stage.
+        gameSessionBuilder.difficulty(gameDifficultyStage.gameDifficulty());
+
+        if (gameDifficultyStage.gameDifficulty() == GameDifficultyOption.CUSTOM) {
+            gameDifficultyStage = new CustomDifficultyStage();
+            printHandler.printView(gameDifficultyStage);
+            input = inputHandler.readInteger(printHandler, 1, GameDifficultyStage.MAX_LIVE_AMOUNT);
+            gameDifficultyStage.submitDifficulty(input);
+        }
+
+        // Building game session after choosing stage.
+        gameSessionBuilder.lives(gameDifficultyStage.getLivesAmount());
+    }
+
+    public void RunGameStages() {
+        GameSession gameSession = gameSessionBuilder.build();
+        printHandler.printView(gameSession);
+        GameStepOption gameStep = GameStepOption.GAMING_STEP;
+        while (gameStep != GameStepOption.RESULTING_STEP) {
+            char inputCharacter = inputHandler.readLetter(printHandler);
+            gameStep = gameSession.executeGameStep(inputCharacter);
+            printHandler.printView(gameSession);
+        }
+
+        GameSessionResults gameSessionResults = new GameSessionResults(gameSession.getGameResults());
+        printHandler.printView(gameSessionResults);
+    }
+
+    private final PrintHandler printHandler;
+    private final InputHandler inputHandler;
+    private final GameSession.GameSessionBuilder gameSessionBuilder = GameSession.builder();
+}
